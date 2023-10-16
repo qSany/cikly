4885a061b8a5e0f64b208f4202e09fb2c370ce1e
admin-rez <kraken_1111@mail.ru>
2023-10-16 14:49:26 +0300

    Домашка здана

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 3");
+        for (int i = 0; i < 18; i = i + 2) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 5");
+        for (int year = 1904; year < 2096; year = year + 4) {
+
+            System.out.println(year + "год является високосным.");
+        }
+        System.out.println("Задача 6");
+        for (int i = 7; i < 100; i = i + 7) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 7");
+        for (int i = 1; i < 513; i = i * 2) {
+            System.out.println(i);
+        }
+        System.out.println("Задача 8");
+        int Deposit = 29000;
+        int  monthlyDeposit = 0;
+        for (int month = 1; month <= 12; month++) {
+            monthlyDeposit = Deposit/12;
+            System.out.println("Месяц " + Deposit + "Сумма накоплений равна" + monthlyDeposit + "рублей");
+        }
+        System.out.println("Задача 9");
+
+
+        for (int i = 1; i < 12; i++) {
+            monthlyDeposit = Deposit/100;
+            monthlyDeposit = monthlyDeposit + Deposit;
+            System.out.println("Месяц " + Deposit + "Сумма накоплений равна" + monthlyDeposit + "рублей");
+
     }
+        System.out.println("Задача 10");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2 x " + i + " = " + 2 * i);
+        }
+}
 }
